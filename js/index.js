6ae7348b45a8b8b03b71d4237784e5aa6f4a6e11
Rosália Fernandes <jessicarosalia678@gmail.com>
2020-07-24 12:19:24 -0300

    chore: Alterando unidades de medida, corrigindo caminho da img do menu e renomeando sua classe, bem como consertando-a no js. Começando a criação da main.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function btn_menu() {
-    const tracos = document.querySelector('img.m')
+    const tracos = document.querySelector('img.img-menu')
     const nav = document.querySelector('nav')
     if (nav.classList) {
         nav.classList.toggle('menu')
