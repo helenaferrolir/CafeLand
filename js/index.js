62e2e3cf2130fad88c5769809400fe36042265a1
Rosália Fernandes <jessicarosalia678@gmail.com>
2020-08-01 10:41:57 -0300

    chore: página home responsiva

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
 function btn_menu() {
     const tracos = document.querySelector('img.img-menu')
     const nav = document.querySelector('nav')
-    if (nav.classList) {
-        nav.classList.toggle('menu')
+
+    const menuDesktop = document.querySelector('.menu-desktop')
+
+    if (nav.classList.contains('menu') != true) {
+        nav.classList.add('menu')
+        menuDesktop.style.display = 'block'
     } else {
-        nav.className += 'menu'
+        nav.classList.remove('menu')
+        menuDesktop.style.display = 'none'
     }
+
+
+}
+
+function refresh() {
+    //screen.width 
+    const menuDesktop = document.querySelector('.menu-desktop')
+    window.addEventListener('resize', function () {
+        if (window.innerWidth >= 850) {
+            menuDesktop.style.display = 'flex'
+        } else {
+            menuDesktop.style.display = 'none'
+        }
+    })
+
 }
