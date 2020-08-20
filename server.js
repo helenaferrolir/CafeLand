d89ea8473f02c0a253d6b8ccc7ed62d7ce8f2350
Rosália Fernandes <jessicarosalia678@gmail.com>
2020-08-20 09:29:38 -0300

    Trazendo o frontend do site para o servidor

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
+server.use(express.static("public"))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", { express: server })
+
+
 server.get("/", function (req, res) {
-    return res.send("hi, baralho")
+    return res.render("index")
 })
+
+
+server.get("/dicas-gerais", function (req, res) {
+    return res.render("dicas-gerais")
+})
+
 
 server.listen(5000, function () {
     console.log("running")
